0dbb13ecc1b0c422c8860faf7eb89ff8e8f532ea
Dragos Dumitrescu <dumitrescu.dragos1@gmail.com>
2014-06-09 15:12:23 +0300

    ajax animation

--- unhcr_frontend/templates/node--article--menu_item.tpl.php
+++ unhcr_frontend/templates/node--article--menu_item.tpl.php
@@ -19,7 +19,11 @@
     print render($content);
     ?>
   </div>
-  <button type="button" class="btn btn-default pull-right" onclick="jQuery('#mini-panel-financial_information_content').load('/node/get/ajax/<?php print $node->nid; ?>');">
+  <button type="button" class="btn btn-default pull-right" onclick="
+      jQuery('#mini-panel-financial_information_content').load('/node/get/ajax/<?php print $node->nid; ?>',function(){
+            jQuery(this).slideDown();
+      });
+  ">
     <span class="glyphicon glyphicon-plus"></span>
   </button>
 </div>
